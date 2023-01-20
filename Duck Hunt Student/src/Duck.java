1214f4ff4274d66a6af4528523858180593bc858
RadonUmar <85165167+RadonUmar@users.noreply.github.com>
2023-01-20 11:05:34 -0800

    Update Duck.java

--- Duck Hunt Student/src/Duck.java
+++ Duck Hunt Student/src/Duck.java
@@ -2,59 +2,29 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Toolkit;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.awt.geom.AffineTransform;
 import java.net.URL;
-
 public class Duck{
-	
-	//add location attributes
-	private int x,y; 	//position of the bird
-	private int vx, vy; //for movement
 	private Image img; 	
 	private AffineTransform tx;
-
+	private int x, y; //position of the bird
+	private int vx, vy; //for movement
+	private boolean shot = false;;
+	private int difficulty = 0;
+	private boolean astronaut = false;
+	
 	public Duck() {
-		img = getImage("/imgs/duck.gif"); //load the image for Duck
+		img = getImage("/Alien/alien-static.png"); //load the image for Tree
+		
+		difficulty = 5;
+		
+		x = (int)(Math.random() * 401) + 100;
+		y = (int)(Math.random() * 401) + 100;
+		
 		tx = AffineTransform.getTranslateInstance(x, y);
-		init(x, y); 				//initialize0 the location of the image
-									//use your variables
-		
-		//give the duck a random non-zero velocity between -3 and 3
-		// in both x and y direction
-		int max = 3;
-		int min = -3;
-		
-		vx = (int)(Math.random() * (max-min+1))+min;
-		vy = (int)(Math.random() * (max-min+1))+min;
-		
-		//what happens if vx or vy were initialized to 0?
-		//how to random their value?
-		while(vx == 0) {
-			//retry until value generated is not 0!
-			vx = (int)(Math.random() * (max-min+1))+min;
-		}
-		
-		//while loop says to keep trying until vy is non-zero
-		while(vy == 0) {
-			//retry until value generated is not 0!
-			vy = (int)(Math.random() * (max-min+1))+min;
-		}
-	}
-	
-	//include a constructor that allows specifying the file name of
-	//the image!
-	public Duck(String fileName) {
-		img = getImage("/imgs/" + fileName); //load the image for Duck
-		tx = AffineTransform.getTranslateInstance(x, y);
-		init(x, y); 				//initialize0 the location of the image
+		init(x, y); 				//initialize the location of the image
 									//use your variables
 	}
-	
-	
-	
-	
 	
 	public void changePicture(String newFileName) {
 		img = getImage(newFileName);
@@ -64,27 +34,87 @@ public class Duck{
 	public void paint(Graphics g) {
 		//these are the 2 lines of code needed draw an image on the screen
 		Graphics2D g2 = (Graphics2D) g;
-		if(this.x > 900) {
-			x--;
+		if (!shot && !astronaut) {
+			
+			// Sporatic Movement
+			/*
+			vy = difficulty * (2 * ((int) (Math.random() * 2)) - 1);
+			vx = difficulty * (2 * ((int) (Math.random() * 2)) - 1);
+			*/
+			
+			
+			// Smooth Movement
+			if(vx > difficulty) {
+				vx = difficulty;
+			}else {
+				if(vx < -difficulty) {
+					vx = -difficulty;
+				}else {
+					vx += (int) (Math.random() * difficulty * 2) - difficulty - 1;
+				}
+			}
+			if(vy > difficulty) {
+				vy = difficulty;
+			}else {
+				if(vy < -difficulty) {
+					vy = -difficulty;
+				}else {
+					vy += (int) (Math.random() * difficulty * 2) - difficulty - 1;
+				}
+			}
+			
+			// Boundaries
+			if((x < 0 && vx < 0) || (x > 675 && vx > 0)) {
+				vx *= -1;
+			}
+			if((y < 0 && vy < 0) || (y > 545 && vy > 0)) {
+				vy *= -1;
+			}
+			
+		} else if (shot && !astronaut) {
+			vx = 0;
+			vy = 10;
+			img = getImage("/Alien/shot-alien-static.png");
 		}
-		x++;
-		//call update to update the actual picture location
+		
+		x += vx;
+		y += vy;
 		update();
+		
 		g2.drawImage(img, tx, null);
-
 	}
 	
-	/* update the picture variable location */
-	private void update() {
+	public void update() {
 		tx.setToTranslation(x, y);
-		tx.scale(.5, .5);
+		tx.scale(0.5, 0.5);
+	}
+	
+	//include a constructor that allows for specifying the file name of the image
+	public Duck(String fileName) {
+		if (fileName.indexOf("Astronaut") != -1 || fileName.indexOf("astronaut") != -1) {
+			astronaut = true;
+		}
+		
+		img = getImage(fileName);
+		tx = AffineTransform.getTranslateInstance(x, y);
+		init(x, y);
+	}
+	
+	public Duck(String fileName, int x, int y) {
+		if (fileName.indexOf("Astronaut") != -1 || fileName.indexOf("astronaut") != -1) {
+			astronaut = true;
+		}
+		
+		img = getImage(fileName);
+		tx = AffineTransform.getTranslateInstance(x, y);
+		init(x, y);
 	}
 	
 	private void init(double a, double b) {
 		tx.setToTranslation(a, b);
 		tx.scale(.5, .5);
 	}
-
+	
 	private Image getImage(String path) {
 		Image tempImage = null;
 		try {
@@ -96,4 +126,39 @@ public class Duck{
 		return tempImage;
 	}
 
+
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public void setVX(int vx) {
+		this.vx = vx;
+	}
+	
+	public void setVY(int vy) {
+		this.vy = vy;
+	}
+	
+	public void setXY(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public boolean isShot() {
+		return shot;
+	}
+	
+	public void setShot(boolean shot) {
+		this.shot = shot;
+	}
+	
+	public void setDifficulty(int difficulty) {
+		this.difficulty = difficulty;
+	}
+	
+	
 }
